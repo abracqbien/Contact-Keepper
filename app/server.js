db7f8531c517be62307916d56d88eb59598abe78
Alexandre <abracqbien.pro@laposte.net>
2019-07-07 16:36:07 +0200

    MongoDB Connected

--- app/server.js
+++ app/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
+
+// CONNECT DATABASE
+connectDb();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
