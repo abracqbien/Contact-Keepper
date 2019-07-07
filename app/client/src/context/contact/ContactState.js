f09c84e2a8d627ca01fcb68aea9f61ec356c9435
Alexandre <abracqbien.pro@laposte.net>
2019-07-07 22:41:15 +0200

    Contact Filter Form & State (NOK)
    Error when filtered get value

--- app/client/src/context/contact/ContactState.js
+++ app/client/src/context/contact/ContactState.js
@@ -49,7 +49,8 @@ const ContactState = props => {
         phone: '111-111-1111'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -93,8 +94,17 @@ const ContactState = props => {
   };
 
   // Filter Contacts
+  const filterContacts = text => {
+    dispatch({
+      type: FILTER_CONTACTS,
+      payload: text
+    });
+  };
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
@@ -105,7 +115,10 @@ const ContactState = props => {
         updateContact,
         current: state.current,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        filtered: state.filtered,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
